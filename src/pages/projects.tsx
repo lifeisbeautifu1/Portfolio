9811ae36147b649c42a57c7b084242121d4c75f2
Potato <poltoradnevone@gmail.com>
2023-01-23 20:22:54 +0400

    fixed bug

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -12,43 +12,43 @@ export default function Projects() {
       </Head>
 
       <div className="h-0 flex-grow cursor-text overflow-y-scroll whitespace-nowrap bg-[#151515] py-1 font-mono font-medium">
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['1']">
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['1']">
           <span className="text-[#74985D]">{"/*"}</span>
         </div>
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['2']">
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['2']">
           <span className="pl-6 text-[#74985D]">
             I have a bunch of projects you can check them out in my github
             profile.
           </span>
         </div>
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['3']">
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['3']">
           <span className="pl-6 text-[#74985D]">
-            I only show you this one because this one is the most advanced i
-            have
+            This discord clone is the most recent and advanced one. Still under
+            the construction 🚧 🏗️ ⛔.
           </span>
         </div>
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['']">
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['']">
           <span className="pl-6 text-[#74985D]">
-            yet made and i'm very proud of myself 🥹.
+            You can check the progress in readme file
           </span>
         </div>
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['4']">
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['4']">
           <span className="text-[#74985D]">{"*/"}</span>
         </div>
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['5']"></div>
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['6']">
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['5']"></div>
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['6']">
           <span className="text-[#679AD1]">const </span>
           <span className="text-[#6EBDF7]">projects </span>=
           <span className="text-[#F7D648]"> {"["}</span>
         </div>
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['7']">
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['7']">
           <span className="pl-6 text-[#CA75CF]">{"{"}</span>
         </div>
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['8']">
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['8']">
           <span className="pl-12 text-[#AADAFA]">name:</span>
           <span className="text-[#C5947C]"> "Discord"</span>,
         </div>
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['9']">
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['9']">
           <span className="pl-12 text-[#AADAFA]">description:</span>
           <span className="text-[#C5947C]">
             {" "}
@@ -56,7 +56,7 @@ export default function Projects() {
           </span>
           ,
         </div>
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['10']">
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['10']">
           <span className="pl-12 text-[#AADAFA]">repository: </span>
           <span className="text-[#C5947C]">"</span>
           <Link
@@ -68,40 +68,40 @@ export default function Projects() {
           </Link>
           <span className="text-[#C5947C]">"</span>,
         </div>
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['11']">
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['11']">
           <span className="pl-12 text-[#AADAFA]">link:</span>
           <span className="text-[#C5947C]"> "coming soon"</span>,
         </div>
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['12']">
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['12']">
           <span className="pl-12 text-[#AADAFA]">stack:</span>
           <span className="text-[#4997EF]">{" {"}</span>
         </div>
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['13']">
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['13']">
           <span className="pl-[4rem] text-[#AADAFA]">frontend:</span>
           <span className="text-[#F7D648]"> {"["}</span>
         </div>
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['14']">
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['14']">
           <span className="pl-[5rem] text-[#C5947C]">"React"</span>,
         </div>
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['15']">
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['15']">
           <span className="pl-[5rem] text-[#C5947C]">"TypeScript"</span>,
         </div>
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['16']">
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['16']">
           <span className="pl-[5rem] text-[#C5947C]">"TailwindCSS"</span>,
         </div>
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['17']">
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['17']">
           <span className="pl-[5rem] text-[#C5947C]">"HeadlessUI"</span>,
         </div>
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['18']">
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['18']">
           <span className="pl-[5rem] text-[#C5947C]">"Framer Motion"</span>,
         </div>
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['19']">
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['19']">
           <span className="pl-[5rem] text-[#C5947C]">"Redux Toolkit"</span>,
         </div>
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['20']">
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['20']">
           <span className="pl-[4rem] text-[#F7D648]">{"]"}</span>,
         </div>
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['21']">
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['21']">
           <span className="pl-[4rem] text-[#AADAFA]">backend:</span>
           <span className="text-[#F7D648]"> {"["}</span>
           <span className="text-[#C5947C]">"Nest.js"</span>,
@@ -111,82 +111,82 @@ export default function Projects() {
           <span className="text-[#C5947C]"> "WebSockets"</span>
           <span className="text-[#F7D648]">{"]"}</span>,
         </div>
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['22']">
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['22']">
           <span className="pl-[4rem] text-[#AADAFA]">devOpes:</span>
           <span className="text-[#F7D648]"> {"["}</span>
           <span className="text-[#C5947C]">"Docker"</span>
           <span className="text-[#F7D648]">{"]"}</span>,
         </div>
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['23']">
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['23']">
           <span className="pl-12 text-[#4997EF]">{"}"}</span>,
         </div>
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['24']">
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['24']">
           <span className="pl-12 text-[#AADAFA]">features:</span>
           <span className="text-[#4997EF]">{" ["}</span>
         </div>
-        <div className="pl-10 text-lg text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['25']">
+        <div className="pl-10 text-lg text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['25']">
           <span className="pl-[4rem] text-[#C5947C]">
             "Sign Up / Sign In / Sign Out / Reset Password / Confirm Email"
           </span>
           ,
         </div>
-        <div className="pl-10 text-lg text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['26']">
+        <div className="pl-10 text-lg text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['26']">
           <span className="pl-[4rem] text-[#C5947C]">
             "Search For Friends / Add Them / Delete Them"
           </span>
           ,
         </div>
-        <div className="pl-10 text-lg text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['27']">
+        <div className="pl-10 text-lg text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['27']">
           <span className="pl-[4rem] text-[#C5947C]">
             "Create Conversations With Users / Create Group Conversations"
           </span>
           ,
         </div>
-        <div className="pl-10 text-lg text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['28']">
+        <div className="pl-10 text-lg text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['28']">
           <span className="pl-[4rem] text-[#C5947C]">
             "Send Messages To Other Users"
           </span>
           ,
         </div>
-        <div className="pl-10 text-lg text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['29']">
+        <div className="pl-10 text-lg text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['29']">
           <span className="pl-[4rem] text-[#C5947C]">"Call Other Users"</span>,
         </div>
-        <div className="pl-10 text-lg text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['30']">
+        <div className="pl-10 text-lg text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['30']">
           <span className="pl-[4rem] text-[#C5947C]">
             "See Incoming Messages / Friends Requests / Calls Live"
           </span>
           ,
         </div>
-        <div className="pl-10 text-lg text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['31']">
+        <div className="pl-10 text-lg text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['31']">
           <span className="pl-[4rem] text-[#C5947C]">
             "See Online Users Live"
           </span>
           ,
         </div>
-        <div className="pl-10 text-lg text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['32']">
+        <div className="pl-10 text-lg text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['32']">
           <span className="pl-[4rem] text-[#C5947C]">
             "See Typing Indicator"
           </span>
           ,
         </div>
-        <div className="pl-10 text-lg text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['33']">
+        <div className="pl-10 text-lg text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['33']">
           <span className="pl-[4rem] text-[#C5947C]">
             "Maintain Privacy (Can't Read Others Chats/Msgs)"
           </span>
           ,
         </div>
 
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['34']">
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['34']">
           <span className="pl-12 text-[#4997EF]">{"]"}</span>,
         </div>
 
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['35']">
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['35']">
           <span className="pl-6 text-[#CA75CF]">{"}"}</span>,
         </div>
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['36']">
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['36']">
           <span className="text-[#F7D648]">{"]"}</span>;
         </div>
-        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-gray-300/80 before:content-['37']"></div>
+        <div className="pl-10 text-lg  text-white before:inline-block before:w-[60px] before:pr-10 before:text-right before:text-gray-300/80 before:content-['37']"></div>
       </div>
     </>
   );
